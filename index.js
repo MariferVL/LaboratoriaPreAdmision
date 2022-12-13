29d1779a5de326e450145b2b2c9784a52b899e44
Marifer VL <mfvl.1186@gmail.com>
2022-12-13 14:55:45 -0300

    radio buttons agregados con sus atributos

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
+function iniciarJuego(){let botoncualeslapregunta=document.getElementById('boton-pelicula')
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+let botonFuego=document.getElementById('boton-fuego')
+botonFuego.addEventListener('click',ataqueFuego)
+let botonAgua=document.getElementById('boton-agua')
+botonAgua.addEventListener('click',ataqueAgua)
+let botonTierra=document.getElementById('boton-tierra')
+botonTierra.addEventListener('click',ataqueTierra)}
 
 if (color === "Café") {
     puntaje =+ 1
