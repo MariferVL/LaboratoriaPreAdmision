02728dda549f20c3f88b01d73fe52c68713004aa
Gaabs97 <ggaabrielagomez@gmail.com>
2022-12-15 00:25:01 -0600

    cambios de puntajes y botones

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-function respuesta() {
-    document.getElementById("respuesta1").innerHTML = "Todas las anteriores";
-   }
+// Caja para guardar nombre 
 
-function mostrarValor(elemento) {
-    alert(elemento.value);
-}
-function darRespuesta(){
-    document.getElementById("respuesta1").innerHTML = "Todas las anteriores";
+function guardarNombre() {
+  var nombreJugador = document.getElementById("nombre");
+  if (nombreJugador.value != "") {
+    alert("¡Hola " + nombreJugador.value + "!");
+  }
 }
 
-function obtenerResultados() {
+// function respuesta() {
+//  document.getElementById("respuesta1").innerHTML = "Todas las anteriores";
+//} 
 
-    //Respuestas Correctas
-    var puntaje = 0;
-  
-  // Loop de preguntas
-    for(var numPreg = 1; numPreg <= 8; numPreg++) {
-      var opcion = document.getElementsByName("opcion"+numPreg);
+// function mostrarValor(elemento) {
+//  alert(elemento.value);
+//}
+//function darRespuesta() {
+//  document.getElementById("respuesta1").innerHTML = "Todas las anteriores";
+//}
 
-  //loop LOOP FOR CHECKING ANSWERS INSIDE EACH RADIO
-      for(var j = 0; j < opcion.length; j++) {
-        var radiosValue = opcion[j];
-        if(radiosValue.value == "correct" && radiosValue.checked) {
-          puntaje++;
-          radiosValue.style.color = "green";
-        }
+
+
+function obtenerResultados () {
+
+  //Respuestas correctas
+  var respuestasCorrectas = 0;
+
+  // Loop de preguntas 
+  for (var pregunta = 1; pregunta <= 5; pregunta++) {
+    var numeroPregunta = document.getElementsByName("opcion" + pregunta);
+
+    //Loop para respuestas
+    for (var respuesta = 0; respuesta < numeroPregunta.length; respuesta++) {
+      var numeroRespuesta = numeroPregunta[respuesta];
+      if (numeroRespuesta.value == "correcto" && numeroRespuesta.checked) {
+        respuestasCorrectas++;
       }
     }
-  document.getElementById('results').innerHTML =
-  "Respuestas correctas " + puntaje;
-  
   }
-  
+  document.getElementById("resultadosFinal").innerHTML =
+    "Respuestas correctas " + respuestasCorrectas;
+}
+
+
+function obtenerResultados2 () {
+
+  //Respuestas correctas
+  var respuestasCorrectas2 = 0;
+
+  // Loop de preguntas 
+  for (var preguntas = 1; preguntas <= 5; preguntas++) {
+    var numeroPreguntas = document.getElementsByName("opciones" + preguntas);
+
+    //Loop para respuestas
+    for (var respuestas = 0; respuestas < numeroPreguntas.length; respuestas++) {
+      var numeroRespuestas = numeroPreguntas[respuestas];
+      if (numeroRespuestas.value == "correcto" && numeroRespuestas.checked) {
+        respuestasCorrectas2++;
+      }
+    }
+  }
+  document.getElementById("resultadosFinal2").innerHTML =
+    "Respuestas correctas " + respuestasCorrectas2;
+}
+
+
+//  Link de donde sacamos el código:
+//  https://teamtreehouse.com/community/javascript-project-simple-quiz-with-radio-buttons
